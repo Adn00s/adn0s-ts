627c1747026d4a13231eda08ad32c554ff08d556
Adnan Labiadh <89914241+Adn00s@users.noreply.github.com>
2021-11-19 05:55:21 +0100

    feat: add install sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt --assume-yes install curl wget git zsh
+curl -sLf "https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh" | sh
+curl -sLf https://spacevim.org/install.sh | bash
