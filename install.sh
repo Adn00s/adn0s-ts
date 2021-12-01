c10499f03cd692cae13f8f247c38a57239b69fb0
Adnan Labiadh <89914241+Adn00s@users.noreply.github.com>
2021-12-01 06:59:29 +0100

    vimrc

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 sudo apt-get update
 sudo apt --assume-yes install curl wget git zsh
 curl -sLf "https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh" | sh
-curl -sLf https://spacevim.org/install.sh | bash
+mkdir -p ~/.vim ~/.vim/autoload ~/.vim/backup ~/.vim/color ~/.vim/plugged
+curl -sLf "" -o ~/.vimrc
